ec3faa5c252a6e9c34a7488ab8dd7259e3543069
jonmetal <antonmas1122@gmail.com>
2026-08-27 17:52:05 +0700

    Init

---
+++ GameWorld/Tiles/TileDefinition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VisualPrototype.GameWorld.Tiles.Structure;
+
+namespace VisualPrototype.GameWorld.Tiles
+{
+    public class TileDefinition
+    {
+        public int Id { get; init; }
+        public string Name { get; init; }
+
+        public StaticTerrain StaticTerrain { get; init; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new VisualPrototype.Game1();
+game.Run();

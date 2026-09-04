eaa399894b2da190f848d484ff60a20382c91208
jonmetal <antonmas1122@gmail.com>
2026-09-04 16:44:22 +0700

    Minor Update

--- GameWorld/Simulation/Chemistry/Element.cs
+++ GameWorld/Simulation/Chemistry/Element.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,8 +12,29 @@ namespace VisualPrototype.GameWorld.Simulation.Chemistry
         public readonly byte AtomicNumber;
         public readonly string Symbol = "";
         public readonly string Name = "";
-
         public readonly decimal AtomicMass;
+        public readonly ElementCategory ElementCategory;
+        public readonly short MeltingPoint;
+        public readonly short BoilingPoint;
+        public readonly decimal Density;
 
+        public readonly byte DefaultValence;
+        public readonly sbyte[] OxidationStates;
+
+        public Element(byte atomicNumber, string symbol, string name, 
+            decimal atomicMass, ElementCategory elementCategory, 
+            short meltingPoint, short boilingPoint, decimal density, byte defaultValence, sbyte[] oxidationStates)
+        {
+            AtomicNumber = atomicNumber;
+            Symbol = symbol;
+            Name = name;
+            AtomicMass = atomicMass;
+            ElementCategory = elementCategory;
+            MeltingPoint = meltingPoint;
+            BoilingPoint = boilingPoint;
+            Density = density;
+            DefaultValence = defaultValence;
+            OxidationStates = (sbyte[])oxidationStates.Clone();
+        }
     }
 }

ebd9c878aff810d82e6f23eecc3c41eeafe8b482
JonMetal <antonmas1122@gmail.com>
2026-09-10 22:50:12 +0700

    Merge1

--- GameWorld/Simulation/Substance.cs
+++ GameWorld/Simulation/Substance.cs
@@ -11,10 +11,6 @@ namespace VisualPrototype.GameWorld.Simulation
     {
         public int Id { get; set; }
 
-<<<<<<< HEAD
         public string Name { get; set; } = ""; 
-=======
-        public string Name { get; set; } = "";
->>>>>>> 13da1e0732682d0d816027db6d8198d8cd6f4ff9
     }
 }
